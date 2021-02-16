9bf867f3e03ff872a18a82bcb489a20f6e7a7fac
dwiknrd <dwiknrd@gmail.com>
2021-02-16 14:59:29 +0700

    update host

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def index():
             return 'FILE UPLOADED SUCCESSFULLY'
     return 'HELLO WORLD NIH'
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
